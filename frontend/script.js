33b61034445469377533c1f37573d7a13b00b1ad
Bacchi <68465815+N35E135@users.noreply.github.com>
2024-12-11 19:09:35 +0900

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("myButton");
-    const message = document.getElementById("message");
-
-    button.addEventListener("click", () => {
-        message.style.display = "block"; // メッセージを表示
-    });
-});
